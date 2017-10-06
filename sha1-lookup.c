255adab495eb05d09eeabd9304b10b89ea999c66
Derrick Stolee <dstolee@microsoft.com>
2017-10-06 09:52:31 -0400

    cleanup: fix possible overflow errors in binary search
    A common mistake when writing binary search is to allow possible
    integer overflow by using the simple average:
    
    	mid = (min + max) / 2;
    
    Instead, use the overflow-safe version:
    
    	mid = min + (max - min) / 2;
    
    The included changes were found using the following two greps:
    
    	grep "/ 2;" *.c
    	grep "/ 2;" */*.c
    	grep "/2;" */*.c
    
    Making this cleanup will prevent future review friction when a new
    binary search is contructed based on existing code.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1-lookup.c
+++ sha1-lookup.c
@@ -95,7 +95,7 @@ int sha1_pos(const unsigned char *sha1, void *table, size_t nr,
 			hi = mi;
 		else
 			lo = mi + 1;
-		mi = (hi + lo) / 2;
+		mi = lo + (hi - lo) / 2;
 	} while (lo < hi);
 	return -lo-1;
 }
